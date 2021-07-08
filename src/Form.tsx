103a17fe6879cd086dd685dd7fa9d3f78a0d97d1
andrew childre <andrewchildre@gmail.com>
2021-07-07 21:21:55 -0600

    centered content

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,53 +1,89 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
-
 const Form = () => {
+	const {
+		register,
+		handleSubmit,
+		getValues,
+		formState: { errors },
+	} = useForm();
 
-    const {register, handleSubmit, formState:{ errors }} = useForm()
+	return (
+		<div className='full-container'>
+			<div  className='left-col' style={{ background: 'red' }}>
+				<form
+					onSubmit={handleSubmit((data) => {
+						console.log(data);
+					})}
+					className='form'>
 
-    return (
-        <div className='full-container'>
-        <div className='container1' style={{background: 'red'}}>
-            <form onSubmit={handleSubmit((data)=>{console.log(data);
-            })} className='form' >
+					<div className='item username'>
+						<label htmlFor='username'>Username:</label>
+						<br />
+						<input
+							{...register('username', { required: 'Username is Required' })}
+							id='username'
+							type='text'
+						/>
+						{errors.username && <p>{errors.username.message}</p>}
+					</div>
 
-               <div className='item username'>
-                <label htmlFor="username">Username:</label>
-                <br />
-                <input {...register('username', {required: 'Username is Required'})} id='username' type="text" />
-                {errors.username && <p>{errors.username.message}</p>}
-               </div>
+					<div className='item email'>
+						<label htmlFor='email'>Email:</label>
+						<br />
+						<input
+							{...register('email', { required: 'Email is Required' })}
+							id='email'
+							type='email'
+						/>
+						{errors.email && <p>{errors.email.message}</p>}
+					</div>
 
-               <div className='item email'>
-                 <label htmlFor="email">Email:</label>
-                 <br />
-                <input {...register('email', {required: 'Email is Required'})} id='email' type="text" />
-                {errors.email && <p>{errors.email.message}</p>}
-                </div>
+					<div className='item password'>
+						<label htmlFor='password'>Password:</label>
+						<br />
+						<input
+							{...register('password', {
+								required: 'Password is Required',
+								minLength: { value: 5, message: 'Must contain 5 characters' },
+							})}
+							type='password'
+							id='password'
+						/>
+						{errors.password && <p>{errors.password.message}</p>}
+					</div>
 
-                <div className='item password'>
-                 <label htmlFor="password">Password:</label>
-                 <br />
-                <input type="text" />
-                </div>
-
-                <div className='item password2'>
-                 <label htmlFor="password2">Confirm Password:</label>
-                 <br />
-                <input type="text" />
-                </div>
-                  <div className='item btn'>
-                <button>Submit</button>
-                </div>
-            </form>
-            
+					<div className='item password2'>
+						<label htmlFor='password2'>Confirm Password:</label>
+						<br />
+						<input
+							type='password'
+							{...register('password2', {
+								required: 'Passwords must match',
+								validate: {
+									matchesPreviousPassword: (value) => {
+										const { password } = getValues();
+										return password === value || 'passwords must match';
+									},
+								},
+							})}
+						/>
+						{errors.password2 && errors.password2.message}
+					</div>
+					<div className='item btn'>
+						<button>Submit</button>
+					</div>
+				</form>
+			</div>
+                            <div className='right-col'>
+			<h1 style={{ background: 'green' }} >
+				Some other shit
+			</h1>
             </div>
-
-            <h1 style={{background: 'green'}} className='right-col'>Some other shit</h1>
-
-            </div>
-    );
+     
+		</div>
+	);
 };
 
 export default Form;
